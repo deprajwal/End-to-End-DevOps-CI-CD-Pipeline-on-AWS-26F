e855b36527184fab834415655afd9233cb82b563
deprajwal <dekateprajwal007@gmail.com>
2026-02-15 11:56:26 +0530

    Update eks.tf

--- terraform_code/eks_code/eks.tf
+++ terraform_code/eks_code/eks.tf
@@ -2,8 +2,9 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
 
-  cluster_name                   = local.name
-  cluster_endpoint_public_access = true
+  cluster_name                    = local.name
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
 
   cluster_addons = {
     coredns = {
@@ -17,17 +18,24 @@ module "eks" {
     }
   }
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     panda-node = {
+
       min_size     = 2
       max_size     = 4
       desired_size = 2
 
-      instance_types = ["t2.medium"]
-      capacity_type  = "SPOT"
+      # t2 not recommended for EKS
+      instance_types = ["t3.large"]
+
+      capacity_type = "SPOT"
+
+      labels = {
+        role = "worker"
+      }
 
       tags = {
         ExtraTag = "Panda_Node"
